7bb31cf79bcabf9cc6e17bbb3f04152e6f6ecf3d
= <=>
2023-03-24 13:08:54 -0700

    Added documentation to preprocessing functions and moved reference tag export to helper_functions.R (though not sure if I did that right)

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,40 +1,23 @@
 
 
-
-
-### deMULTIplex2 ###
-
-# Objectives:
-# 1. Include functionality for MULTI-ATAC barcode sequencing
-# 2. Improve demultiplexing algorithm
-# 3. Improve plotting features
-# 4. Include data-scaling options
-
-# full list of 96 barcodes
-# barcodes <- readxl::read_excel("/Users/dannyconrad/Box/Data/deMULTIplex2/MULTI-seq_oligos_Nov2019.xlsx")$`Barcode Sequence`
-
-
-
-# first 16 barcodes, to move to data
-barcodes <- c("GGAGAAGA","CCACAATG","TGAGACCT","GCACACGC", #1-4
-              "AGAGAGAG","TCACAGCA","GAAAAGGG","CGAGATTC", #5-8
-              "GTAGCACT","CGACCAGC","TTAGCCAG","GGACCCCA", #9-12
-              "CCAACCGG","TGACCGAT","GCAACGCC","CAATCGGT") #13-16
-
-
-
-### Key functions ###
-
-#################
-## revComplement() ##
-#################
-
-# Description:
-# Takes cell barcodes formatted by Cell Ranger and/or ArchR and returns reverse complement of each cell barcode sequence
+#####################
+### revComplement ###
+#####################
+#' Generate Reverse Complement Sequence
+#'
+#' Takes cell barcodes formatted by Cell Ranger and/or ArchR and returns reverse complement of each cell barcode sequence.
+#' Depending on sequencer used, this may be needed to properly align cell barcodes.
+#'
+#' @param cells A character vector of cell barcodes (may include )
+#' @param cbLength Length of cell barcode (default: 16)
+#' @param keepFormat Whether or not to keep non-barcode formatting characters (e.g. "AGTCAGTCAGTCAGTC-1" or "Sample#AGTCAGTCAGTCAGTC")
+#'
+#' @return A character vector
+#'
 #' @export
 revComplement <- function(cells,
-                      cbLength = 16,
-                      keepFormat = F) {
+                          cbLength = 16,
+                          keepFormat = F) {
     require(Biostrings)
     require(stringr)
 
@@ -61,23 +44,49 @@ revComplement <- function(cells,
     return(res)
 }
 
-
-## readTag()
-# previously MULTIseq.preprocess()
-# memory intensive for large FASTQ files
-
-# Description:
-# Locates and reads into memory FASTQ files produced by sequencing MULTIseq or MULTI-ATAC barcode libraries.
-# Read pairs are parsed to extract 10x cell barcodes, MULTI* sample barcodes, and UMIs to count barcodes captured, and these are assembled into a read table
-# Read table can be optionally filtered by a set of known cell barcodes (i.e. cells identified by Cell Ranger)
+###############
+### readTag ###
+###############
+#' Preprocess Tag Reads
+#'
+#' Locates and reads into memory FASTQ files produced by sequencing MULTIseq or MULTI-ATAC barcode libraries.
+#' Read pairs are parsed to extract 10X cell barcodes, MULTI* sample barcodes, and UMIs to count barcodes captured, and these are assembled into a read table.
+#' Read table can be optionally filtered by a set of known cell barcodes (i.e. cells identified by Cell Ranger).
+#' This function is memory intensive for large FASTQ files.
+#' Updated version of MULTIseq.preprocess() from original deMULTIplex package.
+#'
+#' @param dir Path to directory containing tag FASTQ files
+#' @param name Prefix of FASTQ files for the tag library to be processed
+#' @param barcode.type Type of barcoding used: "MULTIseq" or "MULTI-ATAC"
+#' @param assay 10X Genomics single-cell assay used: "RNA", "ATAC", or "Multiome"
+#' @param filter.cells Optional: a character vector of cell barcodes to filter the read table by (default: NULL)
+#' @param fastq.A Optional: provide path to FASTQ containing cell barcodes
+#' @param fastq.B Optional: provide path to FASTQ containing sample tags
+#' @param pos.cbc Optional: numeric vector specifying start & end indices of cell barcode sequence in FASTQ A
+#' @param pos.sbc Optional: numeric vector specifying start & end indices of sample tag sequence in FASTQ B
+#' @param pos.umi Optional: numeric vector specifying start & end indices of UMI sequence in FASTQ A or B (depends on barcode & assay)
+#'
+#' @return Data.frame containing variables "Cell", "UMI", and "Sample"
+#'
+#' @examples
+#' ~/Experiment2
+#' | Exp2MULTI_S3_L002_R1_001.fastq.gz
+#' | Exp2MULTI_S3_L002_R2_001.fastq.gz
+#'
+#' read_table <- readTag(dir = "~/Experiment2",
+#'                       name = "Exp2MULTI",
+#'                       barcode.type = "MULTIseq",
+#'                       assay = "RNA",
+#'                       filter.cells = exp2_cells)
+#'
 #' @export
 readTag <- function(dir,
-                      name,
-                      barcode.type = c("MULTIseq", "MULTI-ATAC"),
-                      assay = c("RNA", "ATAC", "Multiome"),
-                      filter.cells = NULL,
-                      fastq.A = NA, fastq.B = NA,
-                      pos.cbc, pos.sbc, pos.umi) {
+                    name,
+                    barcode.type = c("MULTIseq", "MULTI-ATAC"),
+                    assay = c("RNA", "ATAC", "Multiome"),
+                    filter.cells = NULL,
+                    fastq.A = NA, fastq.B = NA,
+                    pos.cbc, pos.sbc, pos.umi) {
 
     t0 <- Sys.time()
     barcode.type <- match.arg(barcode.type)
@@ -189,24 +198,38 @@ readTag <- function(dir,
 }
 
 
-
-
-
-
-##################
-## alignMULTI() ##
-##################
-
-# previously MULTIseq.align()
-
-# Description:
-# Utilizes data.table library to quickly tally total UMI counts of each sample barcode per cell in read table
+##############
+## alignTag ##
+##############
+#' Generate cell x tag count matrix
+#' Utilizes data.table library to quickly tally total UMI counts of each sample barcode per cell in read table
+#' Updated version of MULTIseq.align() from original deMULTIplex package.
+#'
+#' @param read_table Data.frame containing variables "Cell", "UMI", and "Sample" as output by readTag() function
+#' @param tag.ref Character vector of sample tag sequences used in experiment
+#' @param filter.cells Optional: a character vector of cell barcodes to filter the read table by. Redundant if already filtered with readTag() (default: NULL)
+#' @param string.dist.method Specify method for calculating string distance between reference tags and tag reads when correcting for sequencing errors. See ?stringdist for options (default: hamming)
+#' @param max.dist Specify maximum string distance allowed for correcting sequencing errors in sample tags (default: 1)
+#'
+#' @return Sparse matrix of cell x sample tag counts
+#'
+#' @examples
+#' > head(read_table, n = 3)
+#'             Cell      UMI   Sample
+#' CTTAGGCCTGAACATA TTTCACAC TGAGACCT
+#' TCTGAGCCTAAACTGA CAAAGAGG CCACAATG
+#' TTCTAGACTGAATTGA GATACGCA TGAGACCT
+#'
+#' tag_mtx <- alignTag(read_table,
+#'                     tag.ref,
+#'                     filter.cells = exp2_cells)
+#'
 #' @export
-alignMULTI <- function(read_table,
-                       barcodes,
-                       filter.cells = NULL,
-                       string.dist.method = c("hamming", "osa"),
-                       min.dist = 1) {
+alignTag <- function(read_table,
+                     tag.ref,
+                     filter.cells = NULL,
+                     string.dist.method = "hamming",
+                     max.dist = 1) {
     t0 <- Sys.time()
     string.dist.method <- match.arg(string.dist.method)
     if (is.null(filter.cells)) {
@@ -214,36 +237,36 @@ alignMULTI <- function(read_table,
         cells <- cells[cells != paste(rep("G",16),collapse = "")]
     } else { cells <- filter.cells }
 
-    if(min.dist > 0 ){
-        cat(paste0("Accounting for mismatch with minimal ", string.dist.method, " distance ", min.dist), fill = T)
-        unique_bcs <- unique(read_table$Sample)
-        corrected_bcs<-sapply(unique_bcs, function(x){
-            if(!x %in% barcodes) {
-                x2 = barcodes[which(stringdist(x, barcodes, method = string.dist.method)==min.dist)]
+    if (max.dist > 0 ) {
+        cat(paste0("Accounting for mismatch with maximum ", string.dist.method, " distance ", max.dist), fill = T)
+        unique_tags <- unique(read_table$Sample)
+        corrected_tags<-sapply(unique_tags, function(x){
+            if(!x %in% tag.ref) {
+                x2 = tag.ref[which(stringdist(x, tag.ref, method = string.dist.method)==max.dist)]
                 if(length(x2) == 1) x2 else NA
             } else x
         })
-        read_table$Sample <- corrected_bcs[read_table$Sample]
+        read_table$Sample <- corrected_tags[read_table$Sample]
     }
 
-    cat("Assembling Barcode Count Table...", fill = T)
+    cat("Assembling tag count table...", fill = T)
     dt <- data.table(read_table[,c("Cell", "Sample")])
     dt <- dt[complete.cases(dt), ]
     assign("dt", dt, env = .GlobalEnv)
     cnt_ind <- dt[, list(Freq =.N), by=list(Cell,Sample)]
     cnt_ind$i <- (1:length(cells))[match(cnt_ind$Cell, cells)] # NA?
-    cnt_ind$j <- (1:length(barcodes))[match(cnt_ind$Sample, barcodes)]
+    cnt_ind$j <- (1:length(tag.ref))[match(cnt_ind$Sample, tag.ref)]
     cnt_ind <- cnt_ind[complete.cases(cnt_ind),] # Added to account for provided cell list not in data
-    bc_mtx <- sparseMatrix(i = cnt_ind$i, j = cnt_ind$j, x = cnt_ind$Freq, dims = c(length(cells), length(barcodes)))
-    rownames(bc_mtx) <- cells
-    colnames(bc_mtx) <- barcodes
+    tag_mtx <- sparseMatrix(i = cnt_ind$i, j = cnt_ind$j, x = cnt_ind$Freq, dims = c(length(cells), length(tag.ref)))
+    rownames(tag_mtx) <- cells
+    colnames(tag_mtx) <- tag.ref
 
     cat("Finished in",
         round(difftime(Sys.time(), t0, units = "secs")[[1]]),
         "seconds", sep = " ")
 
     cat("\n")
-    return(bc_mtx)
+    return(tag_mtx)
 }
 
 
